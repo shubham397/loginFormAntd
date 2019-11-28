a25dbaf8dc50167bc4fd9b06ed1a667dddc52289
shubham397 <shubhamkumar397@gmail.com>
2019-11-28 11:10:28 +0530

    firs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Login from './components/loginForm'
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+      <h2>Login Form</h2>
+      <Login/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
